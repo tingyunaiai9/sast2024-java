fe4647f3b573355f77fa3009041516f4fb7c7885
tingyunaiai9 <2920825585@qq.com>
2024-07-31 15:49:45 +0800

    finish hw1
    Signed-off-by: tingyunaiai9 <2920825585@qq.com>

--- src/main/java/homework/ThreeSumClosest.java
+++ src/main/java/homework/ThreeSumClosest.java
@@ -1,15 +1,40 @@
 package homework;
 
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class ThreeSumClosest {
     public static int threeSumClosest(int[] nums, int target) {
         int n = nums.length, ans = 0;
         // TODO begin
 
+        Arrays.sort(nums);
+        int closest = Integer.MAX_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            int left = i + 1;
+            int right = n - 1;
+
+            while (right > left) {
+                int sum = nums[i] + nums[left] + nums[right];
+                int diff = Math.abs(sum - target);
+                if (diff < closest) {
+                    closest = diff;
+                    ans = sum;
+                }
+
+                if (sum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
         // TODO end
         return ans;
     }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int size;
