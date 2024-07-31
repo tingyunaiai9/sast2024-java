9c11191a306f8cf9e0702d00f94581bdc782f2a1
tingyunaiai9 <2920825585@qq.com>
2024-07-31 16:14:10 +0800

    finish hw2 using sliding window
    Signed-off-by: tingyunaiai9 <2920825585@qq.com>

--- src/main/java/homework/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/homework/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,6 +1,8 @@
 package homework;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class LongestSubstringWithoutRepeatingCharacters {
@@ -8,9 +10,22 @@ public class LongestSubstringWithoutRepeatingCharacters {
         int n = s.length(), ans = 0;
         // TODO begin
 
+        Map<Character, Integer> map = new HashMap<>();
+
+        int i = 0;
+
+        for (int j = 0; j < n; j++) {
+            if (map.containsKey(s.charAt(j))) {
+                i = Math.max(map.get(s.charAt(j)) + 1, i);
+            }
+            ans = Math.max(ans, j - i + 1);
+            map.put(s.charAt(j), j);
+        }
+
         // TODO end
         return ans;
     }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the string: ");
